e9a06d4b047cbd806b7251f3d98601ee8aaf1845
Markus Weis <markus.weis1@web.de>
2021-12-20 18:25:32 +0100

    flutter format

--- learning_app/lib/features/timer/models/ticker.dart
+++ learning_app/lib/features/timer/models/ticker.dart
@@ -1,11 +1,10 @@
 import 'package:quiver/async.dart';
 
-
 class Ticker {
   const Ticker();
   Stream<int> tick({required int secs}) {
     DateTime now = DateTime.now();
-    return Metronome.periodic(const Duration(seconds: 1)).map<int>((t) => secs - t.difference(now).inSeconds);
-
+    return Metronome.periodic(const Duration(seconds: 1))
+        .map<int>((t) => secs - t.difference(now).inSeconds);
   }
 }
