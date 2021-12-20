ce78677a6ca6d55879b0ce86a6a9ff28b04bfa2c
Markus Weis <markus.weis1@web.de>
2021-12-20 17:51:17 +0100

    fix/timer-correction-149

--- learning_app/lib/features/timer/models/ticker.dart
+++ learning_app/lib/features/timer/models/ticker.dart
@@ -1,6 +1,11 @@
+import 'package:quiver/async.dart';
+
+
 class Ticker {
   const Ticker();
   Stream<int> tick({required int secs}) {
-    return Stream.periodic(const Duration(seconds: 1), (x) => secs - x - 1);
+    DateTime now = DateTime.now();
+    return Metronome.periodic(const Duration(seconds: 1)).map<int>((t) => secs - t.difference(now).inSeconds);
+
   }
 }
