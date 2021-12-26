63b8ef3d2430ca9165f5069a5a191d5e27b7e7c3
Marc Engelmann <marc.alexander.engelmann@web.de>
2021-12-26 22:52:17 +0100

    feature/task-list-72: Cards-UI

--- learning_app/lib/features/tasks/widgets/task_card.dart
+++ learning_app/lib/features/tasks/widgets/task_card.dart
@@ -22,117 +22,140 @@ class TaskCard extends StatelessWidget {
 
   Widget _card(BuildContext context) {
     const borderRadius = 12.5;
+    var categoryColor =
+        _task.id % 2 == 1 ? Colors.red : Colors.lightBlue.shade600;
+    var checkboxColor =
+        categoryColor; // Colors.green.shade400; // Colors.black54; //categoryColor;
+    // const categoryColor = Colors.lightBlue.shade600;
+
     return Dismissible(
       key: Key(_task.id.toString()),
       onDismissed: (_) =>
           BlocProvider.of<TaskCubit>(context).deleteTaskById(_task.id),
-      child: Card(
-        clipBehavior: Clip.hardEdge,
-        margin: const EdgeInsets.all(0),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(borderRadius),
-        ),
-        color: Colors.white,
-        elevation: 10,
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
-          decoration: BoxDecoration(
-            border: Border(
-              // TODO: Use color of category once added
-              left: BorderSide(width: 12.5, color: Colors.lightBlue.shade600),
-            ),
-            color: Colors.white,
+      child: ColorFiltered(
+        // Grey out when done -> Overlay with semitransparent white; Else
+        // overlay with fulltransparent "black" (no effect)
+        colorFilter: ColorFilter.mode(
+            _task.done ? const Color(0xB8FFFFFF) : const Color(0x00000000),
+            BlendMode.lighten),
+
+        child: Card(
+          clipBehavior: Clip.hardEdge,
+          margin: const EdgeInsets.all(0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
           ),
-          height: 100.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // Checkbox
-              Expanded(
-                flex: 10,
-                child: Transform.scale(
-                  scale: 1.5,
-                  child: Checkbox(
-                    checkColor: Colors.white,
-                    fillColor: MaterialStateProperty.all(
-                      _task.done ? Colors.grey : Colors.lightBlue.shade600,
+          elevation: _task.done ? 2 : 10,
+          color: Colors.white,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+            decoration: BoxDecoration(
+              border: Border(
+                  // TODO: Use color of category once added
+                  left: BorderSide(width: 12.5, color: categoryColor)),
+              color: Colors.white,
+            ),
+            height: 100.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Checkbox
+                Expanded(
+                  flex: 10,
+                  child: Transform.scale(
+                    scale: 1.5,
+                    child: Checkbox(
+                      checkColor: Colors.white,
+                      fillColor: MaterialStateProperty.all(
+                        _task.done ? checkboxColor : categoryColor,
+                      ),
+                      value: _task.done,
+                      shape: const CircleBorder(),
+                      onChanged: (bool? value) {
+                        BlocProvider.of<TaskCubit>(context).toggleDone(_task);
+                      },
                     ),
-                    value: _task.done,
-                    shape: const CircleBorder(),
-                    onChanged: (bool? value) {
-                      BlocProvider.of<TaskCubit>(context).toggleDone(_task);
-                    },
                   ),
                 ),
-              ),
-              const SizedBox(width: 10.0),
-              // Title and Description
-              Expanded(
-                flex: 60,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      _task.title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17.5,
+                const SizedBox(width: 10.0),
+                // Title and Description
+                Expanded(
+                  flex: 60,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        _task.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          decoration: _task.done
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                          decorationThickness: 2.0,
+                          fontSize: 20,
+                        ),
                       ),
-                    ),
-                    const Text("Description of the task"),
-                  ],
+                      // TODO: Use task's real tags (Schlagworte)
+                      Text(_task.id % 3 == 1 ? "Lernen" : "Hausaufgabe"),
+                    ],
+                  ),
                 ),
-              ),
-              // Date and further info
-              Expanded(
-                flex: 30,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Chip(
-                      label: Text(
-                        // TODO: Use real date here
-                        "1. Dez",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      avatar: Icon(
-                        Icons.calendar_today_outlined,
-                        size: 16,
-                        color: Colors.white,
-                      ),
-                      backgroundColor: Colors.purpleAccent,
-                    ),
-                    FittedBox(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Row(
-                            children: const [
-                              Icon(Icons.hourglass_top, size: iconSize),
-                              Text("~ 1 h"),
-                            ],
+                // Date and further info
+                Expanded(
+                  flex: 30,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Chip(
+                          label: Text(
+                            // TODO: Use real date here
+                            _task.id % 2 == 1 ? "Heute" : "1. Dez",
+                            style: TextStyle(
+                                color: _task.id % 2 == 1
+                                    ? Colors.black
+                                    : Colors.white),
                           ),
-                          Container(
-                            margin: const EdgeInsets.only(left: 7.5),
-                            child: Row(
+                          avatar: Icon(
+                            Icons.calendar_today_outlined,
+                            size: 16,
+                            color:
+                                _task.id % 2 == 1 ? Colors.black : Colors.white,
+                          ),
+                          backgroundColor: _task.id % 2 == 1
+                              ? const Color(0x00000000) // Transparent Fakecolor
+                              : Colors.purpleAccent),
+                      FittedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Row(
                               children: const [
-                                Icon(Icons.dynamic_feed_outlined,
-                                    size: iconSize),
-                                SizedBox(width: 5.0),
-                                Text("5 / 8")
+                                Icon(Icons.hourglass_top, size: iconSize),
+                                Text("~ 1 h"),
                               ],
                             ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+                            Container(
+                              margin: const EdgeInsets.only(left: 7.5),
+                              child: Row(
+                                children: const [
+                                  Icon(Icons.dynamic_feed_outlined,
+                                      size: iconSize),
+                                  SizedBox(width: 5.0),
+                                  Text("5 / 8")
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
