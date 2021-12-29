27fa3a99db0f8ce3ff5c1483ad639961b5bff9aa
Timo <timoaccs@gmail.com>
2021-12-29 22:02:29 +0100

    feature/150: Updated documentation: designentscheidungen.md

--- learning_app/lib/features/tasks/persistence/task_entity.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:drift/drift.dart';
-//
-// // Entity for persisting a "Task" model
-// class Tasks extends Table {
-//   IntColumn get id => integer().autoIncrement()();
-//   TextColumn get title => text()();
-//   TextColumn get description => text().nullable()();
-//   BoolColumn get done => boolean()();
-// }
