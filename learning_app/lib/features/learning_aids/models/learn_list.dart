d11017034253453a377f760a8a01b23eb51bb858
Timo <timoaccs@gmail.com>
2022-01-03 23:03:13 +0100

    feature/188: Updated Models for learn-lists

--- learning_app/lib/features/learning_aids/models/learn_list.dart
+++ learning_app/lib/features/learning_aids/models/learn_list.dart
@@ -8,6 +8,26 @@ abstract class LearnList extends Equatable {
   final DateTime creationDate;
   final List<LearnListWord> words;
   final Category? category;
+  final bool isArchived;
+
+  // This value is to be computed within the repository.
+  // It has the purpose to allow accessing this information directly, without
+  // having to prefetch the whole Stream of tasks (via TaskLearnLists)
+  // from within the learn-lists,
+  // just to have (and display) the amount of tasks referencing the
+  // category.
+  // Note that in the other direction, tasks directly contain their learnlists.
+  //
+  // It does not make sense to have to load the whole finished Stream of
+  // tasks just to display the learnLists overview.
+  //
+  // Since Tasks <-> LearnLists is a many-to-many relation, this can easily be
+  // computed by counting the references (e.g. in a relationship-table in a
+  // relational database)
+  // Other than leisure_categories -> leisure_activities, this
+  // many-to-many-table will be relatively small, so a trigger would not be
+  // worth it, here. TODO
+  final int referencingTasksCount;
 
   const LearnList({
     required this.id,
@@ -15,11 +35,14 @@ abstract class LearnList extends Equatable {
     required this.creationDate,
     required this.words,
     this.category,
+    required this.isArchived,
+    required this.referencingTasksCount,
   });
 
   /// Get the count of words in this list
   int get wordsCount => words.length;
 
   @override
-  List<Object?> get props => [id, name, creationDate, words, category];
+  List<Object?> get props =>
+      [id, name, creationDate, words, category, referencingTasksCount];
 }
--- learning_app/lib/features/learning_aids_body_list/models/body_list.dart
+++ learning_app/lib/features/learning_aids_body_list/models/body_list.dart
@@ -3,16 +3,25 @@ import 'package:learning_app/features/learning_aids/models/learn_list.dart';
 import 'package:learning_app/features/learning_aids_body_list/models/body_list_learn_list_word.dart';
 
 class BodyList extends LearnList {
-  const BodyList(int id, String name, DateTime creationDate,
-      List<BodyListLearnListWord> words, Category? category)
-      : super(
+  const BodyList(
+    int id,
+    String name,
+    DateTime creationDate,
+    List<BodyListLearnListWord> words,
+    Category? category,
+    bool isArchived,
+    int referencingTasksCount,
+  ) : super(
           id: id,
           name: name,
           creationDate: creationDate,
           words: words,
           category: category,
+          isArchived: isArchived,
+          referencingTasksCount: referencingTasksCount,
         );
 
   @override
-  List<Object?> get props => [id, name, creationDate, words, category];
+  List<Object?> get props =>
+      [id, name, creationDate, words, category, isArchived];
 }
