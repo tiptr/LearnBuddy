f1a3405eb573ec546b3eca3053c32c9a3dd26e96
Timo Peter <74179345+tiptr@users.noreply.github.com>
2022-01-11 14:50:57 +0100

    Update queue_status.dart

--- learning_app/lib/features/tasks/models/queue_status.dart
+++ learning_app/lib/features/tasks/models/queue_status.dart
@@ -1,8 +1,8 @@
 import 'package:equatable/equatable.dart';
 
 class QueueStatus extends Equatable {
-  final int queuePlacement; // null, if not queued
-  final DateTime addedToQueueDateTime; // null, if not queued
+  final int queuePlacement;
+  final DateTime addedToQueueDateTime;
 
   const QueueStatus(
       {required this.queuePlacement, required this.addedToQueueDateTime});
