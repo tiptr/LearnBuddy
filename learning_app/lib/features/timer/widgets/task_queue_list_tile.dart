201281885cec0943bd4576ab33b83f9a913b19d3
Timo Peter <timoaccs@gmail.com>
2022-01-18 00:31:35 +0100

    feature/131: Merge fixes

--- learning_app/lib/features/timer/widgets/task_queue_list_tile.dart
+++ learning_app/lib/features/timer/widgets/task_queue_list_tile.dart
@@ -19,9 +19,13 @@ class TopLevelListTile extends StatelessWidget {
     final TaskQueueBloc taskQueueBloc = context.read<TaskQueueBloc>();
     int? selectedTaskId;
     TextStyle queueTextStyle = Theme.of(context).textTheme.textStyle2;
-    return BlocBuilder<TaskQueueBloc, TaskQueueState>(
-      builder: (context, state) {
-        return InkWell(
+
+    if (taskQueueBloc.state is TaskQueueReady) {
+      final state = taskQueueBloc.state as TaskQueueReady;
+      selectedTaskId = state.selectedTask;
+    }
+
+    return InkWell(
           child: Text(
             topLevelTaskWithQueueStatus.task.title,
             overflow: TextOverflow.ellipsis,
@@ -38,8 +42,7 @@ class TopLevelListTile extends StatelessWidget {
                 topLevelTaskWithQueueStatus.task.id,
                 topLevelTaskWithQueueStatus.task.id));
           },
-        );
-      },
+
     );
   }
 }
