99ada4106cc8842405d3fc799e3a2a276bd58330
Céline Madeleine Aldenhoven <celine.aldenhoven@tum.de>
2022-01-19 23:44:55 +0100

    stuff

--- learning_app/lib/features/learn_lists/learn_lists_body_list/screens/learning_aid_body_add_screen.dart
+++ learning_app/lib/features/learn_lists/learn_lists_body_list/screens/learning_aid_body_add_screen.dart
@@ -14,7 +14,6 @@ import 'package:learning_app/features/learn_lists/learn_lists_general/widgets/le
 import 'package:learning_app/util/logger.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:learning_app/constants/theme_font_constants.dart';
-import 'package:learning_app/constants/theme_color_constants.dart';
 
 class LearningAidBodyAddScreen extends StatelessWidget {
   const LearningAidBodyAddScreen({Key? key}) : super(key: key);
